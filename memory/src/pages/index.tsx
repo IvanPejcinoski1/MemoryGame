35ac2743c1c9c43879155cbf86f427ee109890d5
Ivan <ivanPejcinoski1@gmail.com>
2024-12-19 15:52:02 +0100

    Added card images

---
+++ memory/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, useContext } from "react";
+const StartGame = () => {
+  return (
+    <div className="container-2xl " id="startGame">
+      StartGame
+    </div>
+  );
+};
+export default StartGame;
